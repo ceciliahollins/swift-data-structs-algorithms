20f96b87a1dc0f93237dd249cc45621de5e05caa
Cecilia Hollins <chollins@deloitte.com.au>
2023-03-29 08:48:28 +1100

    kth to the last node

---
+++ Linked Lists/KthToLastLinkedListNode.playground/Sources/Function.swift
@@ -0,0 +1,40 @@
+
+struct Solution {
+    func kthToLastNode(at k: Int, forHead head: LinkedListNode<Int>) throws -> LinkedListNode<Int> {
+
+        var lastNode: Int = 1
+        var currNode = head
+        while let nextNode = currNode.next {
+            lastNode += 1
+            currNode = nextNode
+        }
+        
+        guard k <= lastNode && k > 0 else {
+                throw LinkedListError.kMoreThanListLength
+            }
+        
+        var i = lastNode - k
+        var kNode = head
+        while i >= 1 {
+            guard let next = kNode.next else { break }
+            kNode = next
+            i -= 1
+        }
+        
+        return kNode
+    }
+}
+
+class LinkedListNode<Value: Equatable> {
+
+    var value: Value
+    var next: LinkedListNode?
+
+    init(_ value: Value) {
+        self.value = value
+    }
+}
+
+enum LinkedListError: Error {
+    case kMoreThanListLength
+}
--- Linked Lists/KthToLastLinkedListNode.playground/Sources/Tests.swift
+++ Linked Lists/KthToLastLinkedListNode.playground/Sources/Tests.swift
@@ -4,21 +4,21 @@ public class Tests: XCTestCase {
     
     func testFirstToLastNode() {
             let nodes = linkedListNodes(from: [1, 2, 3, 4])
-            let actual = try? kthToLastNode(at: 1, forHead: nodes[0])
+        let actual = try? Solution().kthToLastNode(at: 1, forHead: nodes[0])
             let expected = nodes[3]
             XCTAssertTrue(actual === expected)
         }
 
         func testSecondToLastNode() {
             let nodes = linkedListNodes(from: [1, 2, 3, 4])
-            let actual = try? kthToLastNode(at: 2, forHead: nodes[0])
+            let actual = try? Solution().kthToLastNode(at: 2, forHead: nodes[0])
             let expected = nodes[2]
             XCTAssertTrue(actual === expected)
         }
 
         func testFirstNode() {
             let nodes = linkedListNodes(from: [1, 2, 3, 4])
-            let actual = try? kthToLastNode(at: 4, forHead: nodes[0])
+            let actual = try? Solution().kthToLastNode(at: 4, forHead: nodes[0])
             let expected = nodes[0]
             XCTAssertTrue(actual === expected)
         }
@@ -26,7 +26,7 @@ public class Tests: XCTestCase {
         func testKGreaterThanLinkedListLength() {
             let nodes = linkedListNodes(from: [1, 2, 3, 4])
             do {
-                let _ = try kthToLastNode(at: 5, forHead: nodes[0])
+                let _ = try Solution().kthToLastNode(at: 5, forHead: nodes[0])
                 XCTFail()
             } catch {
                 // test passed
@@ -36,7 +36,7 @@ public class Tests: XCTestCase {
         func testKIsZero() {
             let nodes = linkedListNodes(from: [1, 2, 3, 4])
             do {
-                let _ = try kthToLastNode(at: 0, forHead: nodes[0])
+                let _ = try Solution().kthToLastNode(at: 0, forHead: nodes[0])
                 XCTFail()
             } catch {
                 // test passed
