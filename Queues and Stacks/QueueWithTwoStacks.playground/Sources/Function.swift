08988c43efb224aa5856891f1fef314cd1753554
Cecilia Hollins <chollins@deloitte.com.au>
2023-03-30 11:13:33 +1100

    queue with two stacks

---
+++ Queues and Stacks/QueueWithTwoStacks.playground/Sources/Function.swift
@@ -0,0 +1,18 @@
+import Foundation
+
+class Solution<Element> {
+    private var inStack: [Element] = []
+    private var outStack: [Element] = []
+
+    // implement the enqueue and dequeue methods
+    
+
+    func enqueue(_ item: Element) {
+        
+    }
+
+    func dequeue() -> Element? {
+        return nil
+    }
+}
+
