7dc6016a7ec22336aac27cdd544031f08627e63f
Cecilia Hollins <chollins@deloitte.com.au>
2023-04-13 07:43:19 +1000

    update function and add more tests

--- Dictionaries/WordCloudData.playground/Sources/Function.swift
+++ Dictionaries/WordCloudData.playground/Sources/Function.swift
@@ -2,43 +2,75 @@ import Foundation
 
 class Solution {
 
+    private var input: String
     private(set) var wordsToCounts: [String: Int] = [:]
 
     init(_ input: String) {
-        populateWordsToCounts(input)
+        self.input = input
     }
 
-    private func populateWordsToCounts(_ input: String) {
-
-        var string = input
+    private func populateWordsToCounts() {
         
-        while !string.isEmpty {
-            let nextSpace = string.firstIndex(of: " ") ?? string.index(before: string.endIndex) // get next space
-            let startIndex = string.startIndex
-            let substring = string[startIndex...nextSpace] // create the word from substring
-            string.removeSubrange(startIndex...nextSpace) // remove the word from the string
+        // check that the input is not empty and contains words
+        guard !input.isEmpty && input.contains(where: { $0.isLetter() }) else { return }
+        
+        // get the start and end of the first word
+        var wordStart = getWordStart(currWordEnd: input.startIndex)
+        var wordEnd = getWordEnd(currWordStart: wordStart)
+        
+        while wordStart < input.endIndex {
             
-            var word = String(substring)
+            // get the word to populate
+            let wordToPopulate = String(input[wordStart..<wordEnd]).lowercased()
             
-            while !word.isEmpty,
-                  !word.last!.isLetter() {
-                word.removeLast() // clean the word by removing spaces and puncuation
-            }
-            
-            guard !word.isEmpty else { continue }
-            
-            if wordsToCounts[word] != nil {
-                wordsToCounts[word]! += 1
+            // add the word to the dictionary if not already and update the count
+            if let count = wordsToCounts[wordToPopulate] {
+                wordsToCounts[wordToPopulate] = count + 1
             } else {
-                wordsToCounts[word] = 1
+                wordsToCounts[wordToPopulate] = 1
             }
+            
+            // find the start and end of the next word, accounting for puncuation
+            let nextWordStart = wordEnd == input.endIndex ? wordEnd : input.index(after: wordEnd)
+            wordStart = getWordStart(currWordEnd: nextWordStart)
+            wordEnd = getWordEnd(currWordStart: wordStart)
         }
+    }
+    
+    // A helper function to get the start of the next word
+    func getWordStart(currWordEnd: String.Index) -> String.Index {
+        var wordStart = currWordEnd
+        
+        // search through the input until the character is a letter or it is the end of the sentence
+        while wordStart != input.endIndex && !input[wordStart].isLetter() {
+            wordStart = input.index(after: wordStart)
+        }
+        
+        return wordStart
+    }
+    
+    // A helper function to get the end of the current word
+    func getWordEnd(currWordStart: String.Index) -> String.Index {
+        var wordEnd = currWordStart
+        
+        // search through the input until the character is a word splitting character or it is the end of the sentence
+        while wordEnd != input.endIndex && !input[wordEnd].isSplittingWord() {
+            wordEnd = input.index(after: wordEnd)
+        }
+        
+        return wordEnd
     }
 }
 
 extension Character {
+    // A helper function to determine if a character is a letter
     func isLetter() -> Bool {
         return "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ".contains(self)
+    }
+    
+    // A helper function to determine if a character is one that signifies the end of a word
+    func isSplittingWord() -> Bool {
+        return " ,;:.!?:()&\"`".contains(self)
     }
 }
 
--- Dictionaries/WordCloudData.playground/Sources/Tests.swift
+++ Dictionaries/WordCloudData.playground/Sources/Tests.swift
@@ -7,60 +7,132 @@ public class Tests: XCTestCase {
         let wordCloud = Solution(input)
         
         let actual = wordCloud.wordsToCounts
-        let expected = ["I": 1, "like": 1, "cake": 1]
+        let expected = ["i": 1, "like": 1, "cake": 1]
         XCTAssertEqual(actual, expected)
     }
     
     func testLongerSentence() {
         let input = "Chocolate cake for dinner and pound cake for dessert"
         let wordCloud = Solution(input)
-        
+
         let actual = wordCloud.wordsToCounts
         let expected = [
             "and": 1,
             "pound": 1,
             "for": 2,
             "dessert": 1,
-            "Chocolate": 1,
+            "chocolate": 1,
             "dinner": 1,
             "cake": 2
         ]
         XCTAssertEqual(actual, expected)
     }
-    
+
     func testPunctuation() {
         let input = "Strawberry short cake? Yum!"
         let wordCloud = Solution(input)
-        
+
         let actual = wordCloud.wordsToCounts
-        let expected = ["cake": 1, "Strawberry": 1, "short": 1, "Yum": 1]
+        let expected = ["cake": 1, "strawberry": 1, "short": 1, "yum": 1]
         XCTAssertEqual(actual, expected)
     }
-    
+
     func testHyphenatedWords() {
         let input = "Dessert - mille-feuille cake"
         let wordCloud = Solution(input)
-        
+
         let actual = wordCloud.wordsToCounts
-        let expected = ["cake": 1, "Dessert": 1, "mille-feuille": 1]
+        let expected = ["cake": 1, "dessert": 1, "mille-feuille": 1]
         XCTAssertEqual(actual, expected)
     }
-    
+
     func testEllipsesBetweenWords() {
         let input = "Mmm...mmm...decisions...decisions"
         let wordCloud = Solution(input)
-        
+
         let actual = wordCloud.wordsToCounts
         let expected = ["mmm": 2, "decisions": 2]
         XCTAssertEqual(actual, expected)
     }
-    
+
     func testApostrophes() {
         let input = "Allie's Bakery: Sasha's Cakes"
         let wordCloud = Solution(input)
-        
+
         let actual = wordCloud.wordsToCounts
-        let expected = ["Bakery": 1, "Cakes": 1, "Allie's": 1, "Sasha's": 1]
+        let expected = ["bakery": 1, "cakes": 1, "allie's": 1, "sasha's": 1]
+        XCTAssertEqual(actual, expected)
+    }
+    
+    func testParenthesis() {
+        let input = "I (sometimes) like vanilla cake"
+        let wordCloud = Solution(input)
+
+        let actual = wordCloud.wordsToCounts
+        let expected = ["i": 1, "sometimes": 1, "like": 1, "vanilla": 1, "cake": 1]
+        XCTAssertEqual(actual, expected)
+    }
+    
+    func testSingleQuotations() {
+        let input = "In the words of Marie-Antoinette, `let them eat cake`."
+        let wordCloud = Solution(input)
+
+        let actual = wordCloud.wordsToCounts
+        let expected = ["in": 1, "the": 1, "words": 1, "of": 1, "marie-antoinette": 1, "let": 1, "them": 1, "eat": 1, "cake": 1,]
+        XCTAssertEqual(actual, expected)
+    }
+    
+    func testDoubleQuotations() {
+        let input = "He said \"Cake!\""
+        let wordCloud = Solution(input)
+
+        let actual = wordCloud.wordsToCounts
+        let expected = ["he": 1, "said": 1, "cake": 1]
+        XCTAssertEqual(actual, expected)
+    }
+    
+    func testAmpersand() {
+        let input = "Chocolate & vanilla"
+        let wordCloud = Solution(input)
+
+        let actual = wordCloud.wordsToCounts
+        let expected = ["chocolate": 1, "vanilla": 1]
+        XCTAssertEqual(actual, expected)
+    }
+    
+    func testSemiColon() {
+        let input = "She left; I ate cake"
+        let wordCloud = Solution(input)
+
+        let actual = wordCloud.wordsToCounts
+        let expected = ["she": 1, "left": 1, "i": 1, "ate": 1, "cake": 1]
+        XCTAssertEqual(actual, expected)
+    }
+    
+    func testEmpty() {
+        let input = ""
+        let wordCloud = Solution(input)
+
+        let actual = wordCloud.wordsToCounts
+        let expected: [String: Int] = [:]
+        XCTAssertEqual(actual, expected)
+    }
+    
+    func testAllPuncuation() {
+        let input = "?!"
+        let wordCloud = Solution(input)
+
+        let actual = wordCloud.wordsToCounts
+        let expected: [String: Int] = [:]
+        XCTAssertEqual(actual, expected)
+    }
+    
+    func testSentenceStartsWithPuncuation() {
+        let input = "...cake?"
+        let wordCloud = Solution(input)
+
+        let actual = wordCloud.wordsToCounts
+        let expected = ["cake": 1]
         XCTAssertEqual(actual, expected)
     }
 }
