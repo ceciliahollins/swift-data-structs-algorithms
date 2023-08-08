09ae55a5fa2f3e82ed9f4fa7737a798154592ae4
Cecilia Hollins <chollins@deloitte.com.au>
2023-08-09 07:50:32 +1000

    updated funtion and documentation

--- Dynamic Programming and Recursion/RecursiveStringPermutation.playground/Sources/Function.swift
+++ Dynamic Programming and Recursion/RecursiveStringPermutation.playground/Sources/Function.swift
@@ -2,20 +2,28 @@ import Foundation
 
 struct Solution {
     
-    func getPermutations(for inputString: String) -> Set<String> {
+    func getPermutations(for inputString: String) -> Set<String> { 
 
+        // if the input is only one char, only one permutation is possible
         guard inputString.count > 1 else { return [inputString] }
 
+        // create a new set to store permutations
         var permutations: Set<String> = []
         
+        // iterate the chars in the input string
         for (i, char) in inputString.enumerated() {
             
-            var newInputString = inputString
-            newInputString.remove(at: newInputString.index(newInputString.startIndex, offsetBy: i))
+            // update the input string with the current char removed
+            var updatedInputString = inputString
+            let currIndex = updatedInputString.index(updatedInputString.startIndex, offsetBy: i)
+            updatedInputString.remove(at: currIndex)
             
-            let permutationsToAppend = getPermutations(for: newInputString)
+            // recursively get permutation for updated input string
+            let permutationsToAppend = getPermutations(for: updatedInputString)
+            
+            // iterate permutations from recursive call, adding the current char to the front
             for permutation in permutationsToAppend {
-                let possiblePermutation = "\(char)\(permutation)"
+                let possiblePermutation = "\(char)\(permutation)" // TODO: get rid of nested loop
                 permutations.insert(possiblePermutation)
             }
         }
